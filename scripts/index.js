f67cb9c3ca5fce59259090a08404ed89bb01fca3
Ilya Balashov <butch-c@yandex.ru>
2024-02-24 20:12:52 +0300

    first strings

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,32 @@
-// OoO
+const content = document.querySelector('.content');
+const places = content.querySelector('.places');
+const card = places.querySelector('.card');
+// непонятно, как добавляются карточки    const addButton = content.querySelector('')
+const deleteButtonButton = card.querySelector('.card__delete-button');
+
+function addCard(cardTitle, cardImage) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+    cardElement.querySelector('.card__title').textContent = cardTitle;
+    cardElement.querySelector('.card__image').textContent = cardImage; 
+    cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
+     
+      evt.target.classList.toggle('card__like_active');
+  });
+   
+    places.append(cardElement);
+  }
+  // не описан объект addButton !!!!!!
+  addButton.addEventListener('click', function () {
+    const title = document.querySelector('.popup__input_type_card-name');
+    const imageLink = document.querySelector('.popup__input_type_url');
+  
+    addSong(title.value, imageLink.value);
+     
+    title.value = '';
+    imageLink.value = '';
+  });
 // @todo: Темплейт карточки
 
 // @todo: DOM узлы
@@ -6,5 +34,18 @@
 // @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
-
+deleteButton.addEventListener('click', function () {
+    const songs = document.querySelectorAll('.song')
+  
+    for (let i = 0; i < songs.length; i++) {
+      songs[i].remove();
+    }
+  
+    renderNoSongs();
+  });
+  
 // @todo: Вывести карточки на страницу
+
+function initialAddCards() {
+
+}
