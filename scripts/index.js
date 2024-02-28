c7a9e069834333a14feedb426d9e2a63dbdb9671
Ilya Balashov <butch-c@yandex.ru>
2024-02-28 09:57:37 +0300

    before second rewiev

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,37 @@
 
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+const blankCard = cardTemplate.querySelector('.card');
 
 // @todo: DOM узлы
 const content = document.querySelector('.content');
 const placesList = content.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function addCard(cardTitle, cardImage, deleteFunction) {
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+function createCard(cardTitle, cardImage, deleteFunction) {
+  
+  const cardElement = blankCard.cloneNode(true);
+  const cardElementImage = cardElement.querySelector('.card__image')
 
   cardElement.querySelector('.card__title').textContent = cardTitle;
-  cardElement.querySelector('.card__image').src = cardImage; 
-  cardElement.querySelector('.card__image').alt = `Красивый вид ${cardTitle}`; 
-  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunction);        
-  
-  placesList.append(cardElement);
+  cardElementImage.src = cardImage;
+  cardElementImage.alt = `Красивый вид ${cardTitle}`;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunction);
+
+  return(cardElement);
 }
 // @todo: Функция удаления карточки
 function deleteCard(event) {
-  const cardItem = event.target.closest('.card');  
-  console.log(cardItem.name); 
+  const cardItem = event.target.closest('.card');
+  console.log(cardItem.name);
   cardItem.remove();
 }
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (item) {
- 
-  addCard(item.name, item.link, deleteCard);
- 
+  const newCard = createCard(item.name, item.link, deleteCard);
+  placesList.append(newCard);
 });
 
 
 
- 
+
